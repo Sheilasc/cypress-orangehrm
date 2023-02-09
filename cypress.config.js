11361fe415063e47c6eec93327422f54d8b25035
Emilia Carvajal <emilia.carvajalm@gmail.com>
2023-02-08 22:27:47 -0400

    Adding changes to replace hard code url to visit env that is sent from command line

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  e2e: {
+  e2e: {    
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here      
     },
-  },
+  } 
+ 
 };
